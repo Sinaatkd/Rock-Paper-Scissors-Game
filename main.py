80124ff6e01b64d582c3095388f33242ac9e6df7
Sina Amini <general.sina.amini.20@gmail.com>
2024-08-22 14:23:55 +0330

    processing on frame to detect hand

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import cv2
 import mediapipe as mp
 
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
 
 cap = cv2.VideoCapture(0)
 
-while cap.isOpened():
-    ret, frame = cap.read()
+with mp_hands.Hands(max_num_hands=1,
+                    min_detection_confidence=0.75,
+                    min_tracking_confidence=0.75) as detector:
 
-    frame = cv2.flip(frame, 1)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        frame = cv2.flip(frame, 1)
 
-    cv2.imshow('window', frame)
+        # processing on frame to detect hand
+        result_hands = detector.process(frame)
 
-    # close window when q key clicked
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        cv2.imshow('window', frame)
+
+        # close window when q key clicked
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
     
