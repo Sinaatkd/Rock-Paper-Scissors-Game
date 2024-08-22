7d63487da2e941e9bea6423ddce53d402fc7a848
Sina Amini <general.sina.amini.20@gmail.com>
2024-08-22 19:23:22 +0330

    now we can easily play with just one hand

--- game.py
+++ game.py
@@ -1,10 +1,15 @@
 import random
 
 class Game:
-    def __init__(self):
+    def __init__(self, remaining_round=3):
         self.choices = ['rock', 'paper', 'scissors']
         self.computer_choice = None
         self.user_choice = None
+        self.remaining_round = remaining_round
+
+        self.user_score = 0
+        self.computer_score = 0
+        self.draw_count = 0
 
     def set_user_choice(self, choice):
         self.user_choice = choice
@@ -16,21 +21,22 @@ class Game:
     def determine_winner(self):
         """Determines the winner based on the user's and computer's choices."""
         if self.user_choice == self.computer_choice:
-            return "It's a tie!"
+            self.draw_count += 1
         elif (self.user_choice == 'rock' and self.computer_choice == 'scissors') or \
              (self.user_choice == 'paper' and self.computer_choice == 'rock') or \
              (self.user_choice == 'scissors' and self.computer_choice == 'paper'):
-            return "You win!"
+            self.user_score += 1
         else:
-            return "Computer wins!"
+            self.computer_score += 1
+        
+    def playing(self):
+        return self.remaining_round > 0
 
     def ready(self):
         self.get_computer_choice()
 
     def play(self):
         """Plays a round of Rock-Paper-Scissors."""
-        print(f"User chose: {self.user_choice}")
-        print(f"Computer chose: {self.computer_choice}")
-        print(f"------------------------------------------")
         result = self.determine_winner()
+        self.remaining_round -= 1
         return result
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import cv2
 
-from time import sleep
-
 from hand_tracker import HandTracker
 from hand_gesture_recognizer import HandGestureRecognizer
 from game import Game
@@ -16,20 +14,29 @@ is_hand_detected = False
 game = Game()
 
 
-# Font settings
-font = cv2.FONT_HERSHEY_SIMPLEX
-font_scale = 1
-font_thickness = 2
-font_color = (0, 0, 0)  # Black color
+
+frame_rate = cap.get(cv2.CAP_PROP_FPS)
+frame_count = 0
+
+
+def put_text(frame, text, position):
+    # Font settings
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 1
+    font_thickness = 2
+    font_color = (0, 0, 255)  # BGR => Black color
+    cv2.putText(frame, text, position, font, font_scale, font_color, font_thickness)
+
 
 while cap.isOpened():
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
 
     y, x, _ = frame.shape
+    put_text(frame, f"Remaining rounds: {game.remaining_round}", (30, 40))
 
     if not is_hand_detected:
-        cv2.putText(frame, "Wait For Detecting Your Hand", (int(x / 2), 40), font, font_scale, font_color, font_thickness)
+        put_text(frame, "Wait For Detecting Your Hand", (int(x / 2), 40))
 
     # processing on frame to detect hand
     result_hand = hand_detection.process(frame)
@@ -40,7 +47,10 @@ while cap.isOpened():
     if result_hand.multi_hand_landmarks:
         is_hand_detected = True
 
-        game.ready()
+        frame_count += 1
+        frame_timestamp_ms = (frame_count / frame_rate) * 1000
+        if game.playing():
+            put_text(frame, f"{int(frame_timestamp_ms / 1000)}", (int(x / 2), 40))
 
         user_choice = ''
         landmark = result_hand.multi_hand_landmarks[0].landmark
@@ -52,16 +62,29 @@ while cap.isOpened():
             user_choice = "scissors"
         else:
             continue
-        game.set_user_choice(user_choice)
-        cv2.putText(frame, f"Your Chose: {user_choice}", (30, 40), font, font_scale, font_color, font_thickness)
-        cv2.putText(frame, f"Computer Chose: {game.computer_choice}", (30, 100), font, font_scale, font_color, font_thickness)
-        game_result = game.play()
 
+        # run this code 3 seconds after hand detected
+        if frame_timestamp_ms >= 3000 and game.playing():
+            game.ready()
+            game.set_user_choice(user_choice)
+            game_result = game.play()
+            frame_count = 0
+            frame_timestamp_ms = 0
     else:
         is_hand_detected = False
+        frame_count = 0 # reset timer
+
+    put_text(frame, f"Your Chose: {game.user_choice}", (30, 100))
+    put_text(frame, f"Computer Chose: {game.computer_choice}", (30, 140))
+    put_text(frame, f"your score: {game.user_score}", (30, y-20))
+    put_text(frame, f"computer score: {game.computer_score}", (30, y-60))
+    put_text(frame, f"Draw: {game.draw_count}", (30, y-100))
 
     cv2.imshow('window', frame)
 
     # close window when q key clicked
     if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
+cap.release()
+cv2.destroyAllWindows()
