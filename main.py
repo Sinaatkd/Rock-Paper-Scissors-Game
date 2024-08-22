4369d999bae941968f04ee001b60a4845f142fbd
Sina Amini <general.sina.amini.20@gmail.com>
2024-08-22 14:17:52 +0330

    connect to camera and show frame

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("This project is going to be turned into a Rock, Paper, Scissors game using a camera.")
+import cv2
+import mediapipe as mp
+
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    frame = cv2.flip(frame, 1)
+
+    cv2.imshow('window', frame)
+
+    # close window when q key clicked
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    
