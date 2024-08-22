319585f051c30d2bad59bde0480a972c1a01817b
Sina Amini <general.sina.amini.20@gmail.com>
2024-08-22 14:27:02 +0330

    drawing landmarks and hand connections

--- main.py
+++ main.py
@@ -8,6 +8,24 @@ mp_hands = mp.solutions.hands
 
 cap = cv2.VideoCapture(0)
 
+
+def draw_hand_landmarks(frame, result_hand):
+    """draw pose landmarks and connections
+
+    Args:
+        frame
+        result_hand
+    """
+    if result_hand.multi_hand_landmarks:
+        for hand_landmarks in result_hand.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                frame,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+
+
 with mp_hands.Hands(max_num_hands=1,
                     min_detection_confidence=0.75,
                     min_tracking_confidence=0.75) as detector:
@@ -17,7 +35,10 @@ with mp_hands.Hands(max_num_hands=1,
         frame = cv2.flip(frame, 1)
 
         # processing on frame to detect hand
-        result_hands = detector.process(frame)
+        result_hand = detector.process(frame)
+
+        # draw landmarks
+        draw_hand_landmarks(frame, result_hand)
 
         cv2.imshow('window', frame)
 
